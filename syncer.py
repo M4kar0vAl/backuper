bd23a2ebfbed79dea3f827e26cfa1f9a14340bb3
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-29 20:26:40 +0300

    change logging format

--- syncer.py
+++ syncer.py
@@ -7,7 +7,10 @@ import typer
 
 
 log = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] %(message)s",
+)
 
 
 class Syncer:
